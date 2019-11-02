b34924f2c933766345b57095d0fde8afe1bfb89d
DevGaby <sgabrielcalixte@gmail.com>
2019-11-02 12:58:08 +0100

    Home Page

--- VDIConnectAPI/VDIConnect_API/VDIConnect_API/Controllers/HomeController.cs
+++ VDIConnectAPI/VDIConnect_API/VDIConnect_API/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace VDIConnect_API.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
+            ViewBag.Title = "VDIConnect API";
 
             return View();
         }
